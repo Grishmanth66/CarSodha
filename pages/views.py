271957dfd0efb9dc5e5d318a58b66629284bafb2
Grishmanth66 <grishmanth.6@gmail.com>
2025-02-10 16:08:20 +0530

    Gallery changes

--- pages/views.py
+++ pages/views.py
@@ -5,9 +5,11 @@ from cars.models import car
 def home(request):
     teams = Team.objects.all()
     featured_cars = car.objects.order_by('-created_date').filter(is_featured=True)
+    all_cars= car.objects.order_by('-created_date')
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
+        'all_cars': all_cars,
     }
     return render(request, 'pages/home.html', data)
 
